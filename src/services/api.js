20dcf2bc5e49efc35541abd46e5cf4a85457d387
Dmytro Azarenkov <azarenkov.mintymint@gmail.com>
2024-08-30 08:50:41 +0300

    Add rides page

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1/auth';
+const API_URL = 'http://localhost:8000/api/v1';
 
 export const login = async (email, password) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, {
+    const response = await axios.post(`${API_URL}/auth/login`, {
       email,
       password,
     });
@@ -16,5 +16,19 @@ export const login = async (email, password) => {
     }
   } catch (error) {
     throw new Error(error.response?.data?.msg || 'Error during login');
+  }
+};
+
+export const getProfiles = async (token) => {
+  try {
+    const response = await axios.get(`${API_URL}/profile/allprofiles`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data.users;
+  } catch (error) {
+    throw new Error(error.response?.data?.msg || 'Error fetching profiles');
   }
 };
