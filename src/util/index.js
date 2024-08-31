886e76b36aa46cc397b1cf9f580d8fc289c33f74
Maher A <maher.gepe@gmail.com>
2024-08-31 05:02:10 -0700

    token_hook

--- src/util/index.js
+++ src/util/index.js
@@ -1,25 +1,7 @@
-import axios from "axios";
- 
-// note: not used, but could be used with GET with params
- const getData = async (url, params) => {
-    try {
-      let res = await axios.get(url, params);
-      let data = await res.data;
-      return data;
-    } catch (error) {
-      console.log(error, `error - getData in ${url} route`);
-    }
-  };
+const token = localStorage.getItem('authToken');
+const user = localStorage.getItem('authUser');
 
-  const getAllData = async (url) => {
-    try {
-      let res = await axios.get(url);
-      let data = await res.data;
-      return data;
-    } catch (error) {
-      console.log(error, `error - getAllData in ${url} route`);
-    }
-  };
+const credentials = {token:token, user:user}
 
+export default credentials;
 
-  export {getData, getAllData};
